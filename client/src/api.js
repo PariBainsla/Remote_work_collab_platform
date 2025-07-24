b6c8add4fa693a6cd17af5d8d16a0f1d69f4394e
PariBainsla <paribainsla161@gmail.com>
2025-07-24 16:26:03 -0700

    axios

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Attach token automatically to requests
@@ -14,3 +14,4 @@ API.interceptors.request.use((req) => {
 });
 
 export default API;
+
