3ded76e0acf4b3ff8a92520f74362da4ff647d9c
PariBainsla <paribainsla161@gmail.com>
2025-07-24 15:40:15 -0700

    deployed

--- server/server.js
+++ server/server.js
@@ -27,8 +27,15 @@ console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected ✅'))
+})
+  .then(() => {
+    console.log('MongoDB connected ✅');
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    });
+  })
   .catch((err) => console.error('MongoDB connection error ❌', err));
+
 
 
 // Start server
